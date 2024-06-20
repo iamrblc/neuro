0f3fca352ab685fb4e58c11cc36840156b17fcfe
iamrblc <robilaci@gmail.com>
2024-06-20 13:36:01 +0200

    "Setup environment"

---
+++ scripts/audio_stimulus_presenter.m
@@ -0,0 +1,20 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% AUDIO STIMULUS PRESENTER FOR HD-DOT %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Make sure you are in the scripts directory when launching the file.
+
+% 0. ENVIRONMENT SETTINGS
+
+% Clear everything
+clear all
+clc
+
+% Set up current dictionary as working directory ('./')
+cd(fileparts(mfilename('fullpath')))
+
+% Set up path to the audio snippets (../data_audio)
+audio_path = fullfile(fileparts(mfilename('fullpath')), '..', 'data_audio');
+
+disp("It ran until here for sure")
+
